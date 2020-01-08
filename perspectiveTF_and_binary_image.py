300f9613257de70e641c978fd72d68f075d64b02
GuoliangPeng <gl.peng@outlook.com>
2020-01-08 18:08:01 +0800

    finish raw image to binary image parameter test

--- perspectiveTF_and_binary_image.py
+++ perspectiveTF_and_binary_image.py
@@ -1,16 +1,16 @@
 '''
-输入：前置摄像头原始图像
+输入：前置摄像头已经失真矫正图像
 测试：透视变换参数，图片二值化方式及参数
 '''
 import numpy as np
-from cv2 import cv2
+import cv2
 import glob
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import pickle
 
-
-dist_pickle = pickle.load( open( "Demo_camera_cal/wide_dist_pickle.p", "rb" ) )
+'''
+dist_pickle = pickle.load( open( "./Demo_camera_cal/wide_dist_pickle.p", "rb" ) )
 mtx = dist_pickle["mtx"]
 dist = dist_pickle["dist"]
 
@@ -19,18 +19,19 @@ def img_undistort(img,mtx,dist):
     dst = cv2.undistort(img, mtx, dist, None, mtx)
     return dst
 
-'''
 #测试-失真矫正（对原始图像-->失真矫正）
-img = cv2.imread('camera_cal/calibration2.jpg')
+img = mpimg.imread('./20191231test/image0001.jpg')
 dst_img = img_undistort(img,mtx,dist)
-cv2.imwrite('output_images/test_undist.jpg',dst_img)
+#cv2.imwrite('output_images/test_undist.jpg',dst_img)
 
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
 ax1.imshow(img)
 ax1.set_title('Original Image', fontsize=30)
 ax2.imshow(dst_img)
 ax2.set_title('Undistorted Image', fontsize=30)
-plt.savefig('./writeup_images/image1.jpg')
+plt.savefig("./writeup_images/image2.png")
+plt.show()
+'''
 '''
 #透视变换
 def warper(img):
@@ -44,8 +45,8 @@ def warper(img):
     #print(Minv)
     warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_NEAREST)  # keep same size as input image
     return warped,Minv
-'''
-#测试-透视变换参数（对原始直道图像-->失真矫正-->透视变换）
+
+#测试-透视变换参数（对失真矫正图像-->透视变换图像）
 test_img1 = mpimg.imread('test_images/straight_lines1.jpg')
 test_distort_image1 = img_undistort(test_img1,mtx,dist)
 test_top_down_warped1,test_Minv1 = warper(test_distort_image1)
@@ -96,15 +97,24 @@ def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
     return binary_output
 
 # Edit this function to create your own pipeline.
-def pipeline(img, r_thresh=(230, 255), l_thresh=(210,255),s_thresh=(120,255),b_lab_thresh=(220,255),sxy_thresh=(45, 100)):#170, 255
+def pipeline(img, r_thresh=(150, 255), g_thresh=(125,255), h_thresh=(15,120), l_thresh=(210,255),s_thresh=(40,255),l_lab_thresh=(140,200),b_lab_thresh=(225,255),sxy_thresh=(40, 80)):#170, 255
 
     #RGB
     r_rgb = img[:,:,0]
     r_binary = np.zeros_like(r_rgb)
     r_binary[(r_rgb >= r_thresh[0]) & (r_rgb <= r_thresh[1])] = 1
+
+    g_rgb = img[:,:,1]
+    g_binary = np.zeros_like(g_rgb)
+    g_binary[(g_rgb >= g_thresh[0]) & (g_rgb <= g_thresh[1])] = 1
     
     # HLS color space
     hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+
+    h_channel = hls[:,:,0]
+    h_binary = np.zeros_like(h_channel)
+    h_binary[(h_channel >= h_thresh[0]) & (h_channel <= h_thresh[1])] = 1
+
     l_channel = hls[:,:,1]
     l_binary = np.zeros_like(l_channel)
     l_binary[(l_channel >= l_thresh[0]) & (l_channel <= l_thresh[1])] = 1
@@ -118,15 +128,21 @@ def pipeline(img, r_thresh=(230, 255), l_thresh=(210,255),s_thresh=(120,255),b_l
     b_lab = lab[:,:,2]
     if np.max(b_lab) > 100:
         b_lab = b_lab*(255/np.max(b_lab))
-    b_binary = np.zeros_like(b_lab)
-    b_binary[((b_lab > b_lab_thresh[0]) & (b_lab <= b_lab_thresh[1]))] = 1
+    b_lab_binary = np.zeros_like(b_lab)
+    b_lab_binary[((b_lab > b_lab_thresh[0]) & (b_lab <= b_lab_thresh[1]))] = 1
     
+    l_lab = lab[:,:,0]
+    #np.set_printoptions(threshold=np.inf)
+    #print(a_lab)
+    l_lab_binary = np.zeros_like(l_lab)
+    l_lab_binary[((l_lab > l_lab_thresh[0]) & (l_lab <= l_lab_thresh[1]))] = 1
+
     '''
     # Sobel x
     gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     sobelx = cv2.Sobel(gray_img, cv2.CV_64F, 1, 0, ksize=3) # Take the derivative in x
     abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
-    scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))   
+    scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))  
     #Sobel
     
     # Threshold x gradient
@@ -135,27 +151,28 @@ def pipeline(img, r_thresh=(230, 255), l_thresh=(210,255),s_thresh=(120,255),b_l
     '''
     
     sxbinary=abs_sobel_thresh(img,'x',sxy_thresh)
-    dir_binary = dir_threshold(img, sobel_kernel=3, thresh=(0.7, 1.3))
+    dir_binary = dir_threshold(img, sobel_kernel=3, thresh=(0.1, 1.3))
     
     # Stack each channel
     #color_binary = np.dstack(( np.zeros_like(sxbinary), sxbinary, s_binary)) * 255
     color_binary = np.dstack((r_binary, sxbinary, l_binary)) * 255
    # Combine the two binary thresholds
     combined_binary = np.zeros_like(sxbinary)
-    combined_binary[(s_binary==1)&(dir_binary==1)|(r_binary == 1)|(l_binary == 1)|(sxbinary == 1)] = 1#(b_binary == 1)
+    combined_binary[(s_binary==1)&(dir_binary==1)] = 1#(l_lab_binary==1)&(b_lab_binary==1)  (g_binary==1)&(r_binary==1)  (s_binary==1)&(dir_binary==1)  |(sxbinary == 1)&(dir_binary==1)#(s_binary==1)&(dir_binary==1)|(r_binary == 1)|(l_binary == 1)|(sxbinary == 1)#(b_binary == 1)
     return color_binary,combined_binary
 
-'''
 #测试-渐变和色彩空间,输出二值化图像(输入图像为已经过失真矫正，透视变换的图像)
-color_result_test,combined_binary_test = pipeline(top_down_warped_test)
+test_img = mpimg.imread('./20191231test/2.15ma_imgs/1.jpg')
+#color_result_test,combined_binary_test = pipeline(top_down_warped_test)
+color_result_test,combined_binary_test = pipeline(test_img)
 # Plot the result
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
 f.tight_layout()
 
-ax1.imshow(top_down_warped_test)
+ax1.imshow(test_img)
 ax1.set_title('Original Image', fontsize=40)
 ax2.imshow(combined_binary_test,cmap='gray')
 ax2.set_title('Pipeline Result', fontsize=40)
 #plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
-plt.savefig('./writeup_images/image5.jpg')
-'''
+plt.savefig('./writeup_images/test_image.png')
+plt.show()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
